d8c72346cbc4a38c240c9fce6f2e9a13a975023d
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-11-05 09:07:45 -0500

    Initial Commit

---
+++ SwiftUI-CoreData/SwiftUI_CoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CoreDataApp.swift
+//  SwiftUI-CoreData
+//
+//  Created by R. Mark Volkmann on 11/5/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
